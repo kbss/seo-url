5486eec662f3ca6978772bb2258d9d7687e49c40
kbss <kbss@ukr.net>
2021-07-04 23:07:58 +0300

    - readme.md update

--- src/main/java/com/stylight/seo/service/InMemoryUrlService.java
+++ src/main/java/com/stylight/seo/service/InMemoryUrlService.java
@@ -12,7 +12,6 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-//TODO: caching
 @Slf4j
 @Service
 public class InMemoryUrlService implements UrlService {
